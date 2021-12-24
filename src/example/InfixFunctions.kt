dec6d10235fe7099c0842c284bc13fe06febc9bc
tein408 <tein408@gmail.com>
2021-12-24 19:49:52 +0900

    add Infix functions

--- src/example/InfixFunctions.kt
+++ src/example/InfixFunctions.kt
@@ -4,5 +4,22 @@ fun main() {
     infix fun Int.times(str: String) = str.repeat(this)
     println(2 times "Bye")
 
+    val pair = "horse" to "latte"
+    println(pair)
 
+    infix fun String.onto(other: String) = Pair(this, other)
+    val myPair = "Latte" onto "is Horse"
+    println(myPair)
+
+    val sophia = Person("Sophia")
+    val cludia = Person("Claudia")
+    sophia likes cludia
+
+}
+
+class Person(val name : String) {
+    val likedPeople = mutableListOf<Person>()
+    infix fun likes(other: Person) {
+        likedPeople.add(other)
+    }
 }
